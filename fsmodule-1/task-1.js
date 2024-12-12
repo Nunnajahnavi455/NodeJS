e494932136b4580d1bf63051a505758969844ecd
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-12-12 11:53:35 +0530

    task

---
+++ fsmodule-1/task-1.js
@@ -0,0 +1,12 @@
+//read json file and print all user names
+import fs from 'fs'
+let data=fs.readFile('users.json','utf-8',(err,data)=>{
+    if(err) throw err
+    console.log(typeof data)
+    let users=JSON.parse(data)
+    console.log(typeof users)
+    for(let user of users){
+        console.log(user.name)
+    }
+
+})
