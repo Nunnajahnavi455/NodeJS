e4f2a480fe4727b148ff1201ac26283711d88c22
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-12-13 11:13:38 +0530

    users

---
+++ fsmodule-1/json modules/rw.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+fs.readFile('users1.json','utf-8',(err,data)=>{
+    if(err){
+        console.log(`unable to Read - : ${err.message}`)
+    }else{
+        console.log(typeof data)
+        let users =JSON.parse(data)
+        console.log(typeof users)
+        console.log(users.length)
+    }
+})
---
+++ fsmodule-1/rw1.js
@@ -0,0 +1,19 @@
+import fs from 'fs'
+fs.readFile('users.json','utf-8',(err,data)=>{
+    if (err){
+        console.log(`unable to read-${err}`)
+    }
+    else{
+        let users=JSON.parse(data)
+        let Male_users=users.filter(user =>user.gender ==='Male')
+        let Female_users=users.filter(user =>user.gender ==='Female')
+
+        fs.writeFile('Male.json',JSON.stringify(Male_users),(err)=>{
+            fs.writeFile('Female.json',JSON.stringify(Female_users),(err)=>{
+                if (err) throw err
+                console.log('New File Created')
+            })
+        })
+        
+    }
+})
