9b8f6f9f658ed60d1a3bb2d5e08848e71764aeef
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-12-16 11:13:55 +0530

    http

---
+++ HTTP-1/server.js
@@ -0,0 +1,9 @@
+import http from 'http'
+//create server
+let server=http.createServer((req,resp)=>{
+    resp.end("Hello,Good Morning")
+});
+server.listen(8080,'127.0.0.1',(err)=>{
+    if(err) throw err
+    console.log("server is running on port :8080")
+})
